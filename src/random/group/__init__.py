6b483198cf0d735293c6bfbcd10723244ff220de
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 22:55:59 +0300

    feat: initialize random dithering group utilities
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/random/group/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .normal import normal_distribution
+from .uniform import uniform_distrib
+
+__all__ = [
+    "normal_distribution",
+    "uniform_distrib",
+]
