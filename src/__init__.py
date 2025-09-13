7c4cca98e02b69d01bb7a0ec69b2310f327d5856
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 22:55:48 +0300

    feat: initialize main package structure
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from . import adaptive_diffusion, error_diffusion, multi_level, naive, ordered, random
+
+__all__ = [
+    "adaptive_diffusion",
+    "error_diffusion",
+    "multi_level",
+    "naive",
+    "ordered",
+    "random",
+]
