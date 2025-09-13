71265fe115791d288cd8072018cde083cf8f6eb1
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:25 +0300

    feat: add experimental error diffusion kernels
    - Research and development kernels
    - Non-standard diffusion patterns
    - Testing ground for new algorithms
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/experimental.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+EXPERIMENTAL: Dict[str, Kernel] = {
+    "ripple": ([
+        ((0, +1), 4), ((0, +2), 2), ((0, +3), 1),
+        ((+1, -1), 1), ((+1, 0), 2), ((+1, +1), 1),
+        ((+2, 0), 1),
+    ], 12),  # https://jdobr.es/blog/dithering/
+
+    "cluster": ([
+        ((0, +1), 3), ((0, +2), 3),
+        ((+1, 0), 3), ((+1, +1), 3),
+    ], 12),  # https://en.wikipedia.org/wiki/Ordered_dithering
+
+    "serpentine": ([
+        ((0, +1), 6),
+        ((+1, -1), 2), ((+1, 0), 4), ((+1, +1), 2),
+        ((+2, -1), 1), ((+2, +1), 1),
+    ], 16),  # https://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
+
+    "diamond": ([
+        ((0, +1), 4),
+        ((-1, 0), 2),
+        ((+1, -1), 1), ((+1, 0), 2), ((+1, +1), 1),
+    ], 10),  # https://surma.dev/things/ditherpunk/
+
+    "hexagonal": ([
+        ((0, +1), 3), ((0, +2), 1),
+        ((-1, +1), 1),
+        ((+1, -1), 1), ((+1, 0), 3), ((+1, +1), 3),
+    ], 12),  # https://surma.dev/things/ditherpunk/
+}
+
+EXPERIMENTAL_ALIASES: Dict[str, str] = {
+    "RIP": "ripple",
+    "CL": "cluster",
+    "SERP": "serpentine",
+    "DIA": "diamond",
+    "HEX": "hexagonal",
+}
