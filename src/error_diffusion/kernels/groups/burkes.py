8f3ed6790bdd946488613b279ae362a29c8475a7
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:23 +0300

    feat: implement Burkes error diffusion kernel
    - 8-neighbor alternative to Sierra family
    - Balanced error distribution pattern
    - Good performance on detailed images
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/burkes.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+BURKES: Dict[str, Kernel] = {
+    "burkes": ([
+        ((0, +1), 8),  ((0, +2), 4),
+        ((+1, -2), 2), ((+1, -1), 4), ((+1, 0), 8), ((+1, +1), 4), ((+1, +2), 2),
+    ], 32),  # https://tannerhelland.com/2012/12/28/dithering-eleven-algorithms-source-code.html
+}
+
+BURKES_ALIASES: Dict[str, str] = {
+    "B": "burkes",
+}
