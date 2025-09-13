fd9baf84913fefb082c78ddaf7ca63b7c80db550
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:25 +0300

    feat: implement fan-pattern error diffusion
    - Radial error distribution patterns
    - Specialized for circular diffusion effects
    - Alternative to linear kernel arrangements
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/fan.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+FAN: Dict[str, Kernel] = {
+    "fan": ([
+        ((0, +1), 7),
+        ((+1, -1), 1), ((+1, 0), 3), ((+1, +1), 5),
+    ], 16),  # https://doi.org/10.1117/12.236968
+
+    "shiau_fan": ([
+        ((0, +1), 4),
+        ((+1, -1), 1), ((+1, 0), 1), ((+1, +1), 2),
+    ], 8),  # https://doi.org/10.1117/12.236968
+
+    "shiau_fan2": ([
+        ((0, +1), 8),
+        ((+1, -1), 1), ((+1, 0), 2), ((+1, +1), 4),
+    ], 16),  # https://doi.org/10.1117/12.236968
+}
+
+FAN_ALIASES: Dict[str, str] = {
+    "FAN": "fan",
+    "SF": "shiau_fan",
+    "SF2": "shiau_fan2",
+}
