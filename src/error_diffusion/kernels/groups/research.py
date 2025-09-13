3e67a757f8d1549e4081a1c7e7dd5b787dc73bf0
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:27 +0300

    feat: add research error diffusion kernels
    - Academic and paper implementations
    - Cutting-edge diffusion algorithms
    - Experimental quality improvements
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/research.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+RESEARCH: Dict[str, Kernel] = {
+    "bell": ([
+        ((0, +1), 8),  ((0, +2), 4),
+        ((+1, -2), 2), ((+1, -1), 4), ((+1, 0), 8), ((+1, +1), 4), ((+1, +2), 2),
+        ((+2, -1), 1), ((+2, 0), 2),  ((+2, +1), 1),
+    ], 36),  # https://en.wikipedia.org/wiki/Error_diffusion
+
+    "stevenson_arce": ([
+        ((0, +1), 32), ((0, +2), 12),
+        ((+1, -2), 5),  ((+1, -1), 12), ((+1, 0), 26), ((+1, +1), 12), ((+1, +2), 5),
+        ((+2, -2), 2),  ((+2, -1), 5),  ((+2, 0), 12), ((+2, +1), 5),  ((+2, +2), 2),
+    ], 200),  # https://en.wikipedia.org/wiki/Error_diffusion
+}
+
+RESEARCH_ALIASES: Dict[str, str] = {
+    "BELL": "bell",
+    "SA": "stevenson_arce",
+}
