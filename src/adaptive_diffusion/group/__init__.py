403f97c85cb96ef8d026fb6909bab9bb2d993521
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 22:55:49 +0300

    feat: initialize adaptive diffusion group utilities
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/adaptive_diffusion/group/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .ostromoukhov import ostromoukhov_bw
+from .zhou_fang import zhou_fang_bw
+
+__all__ = [
+    "ostromoukhov_bw",
+    "zhou_fang_bw",
+]
