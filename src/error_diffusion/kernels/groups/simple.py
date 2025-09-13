e25911c23705e5271881a5d6b6507660cd10c283
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:28 +0300

    feat: add simple error diffusion kernels
    - Minimal neighbor count patterns
    - Basic 2-4 neighbor implementations
    - Educational and lightweight options
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/simple.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+SIMPLE: Dict[str, Kernel] = {
+    "simple_2d": ([
+        ((0, +1), 1),
+        ((+1, 0), 1),
+    ], 2),  # https://en.wikipedia.org/wiki/Error_diffusion
+
+    "false_floyd_steinberg": ([
+        ((0, +1), 3),
+        ((+1, -1), 1), ((+1, 0), 1),
+    ], 8),  # https://tannerhelland.com/2012/12/28/dithering-eleven-algorithms-source-code.html
+
+    "three_pixel": ([
+        ((0, +1), 2),
+        ((+1, -1), 1), ((+1, 0), 1),
+    ], 4),  # https://numbersandshapes.net/posts/error_diffusion/
+
+    "four_pixel": ([
+        ((0, +1), 2),
+        ((+1, -1), 1), ((+1, 0), 2), ((+1, +1), 1),
+    ], 6),  # https://surma.dev/things/ditherpunk/
+
+    "micro": ([
+        ((0, +1), 1),
+    ], 1),  # https://en.wikipedia.org/wiki/Error_diffusion
+}
+
+SIMPLE_ALIASES: Dict[str, str] = {
+    "S2D": "simple_2d",
+    "FFS": "false_floyd_steinberg",
+    "3P": "three_pixel",
+    "4P": "four_pixel",
+    "M": "micro",
+}
