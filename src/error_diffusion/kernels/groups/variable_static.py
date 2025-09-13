db56f3117450cfbe6f2f82a7cecdf4e12d6609c7
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 23:00:28 +0300

    feat: add static variable error diffusion kernels
    - Pre-computed variable weight patterns
    - Static lookup tables for performance
    - Compromise between adaptive and fixed kernels
    
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/error_diffusion/kernels/groups/variable_static.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from typing import Dict
+
+from .ktype import Kernel
+
+VARIABLE_STATIC: Dict[str, Kernel] = {
+    "ostromoukhov_static": ([
+        ((0, +1), 13),
+        ((+1, -1), 0), ((+1, 0), 5), ((+1, +1), 2),
+    ], 20),  # https://dl.acm.org/doi/10.1145/383259.383326
+
+    "zhou_fang_static": ([
+        ((0, +1), 7),
+        ((+1, -1), 1), ((+1, 0), 3), ((+1, +1), 1),
+    ], 12),  # https://en.wikipedia.org/wiki/Error_diffusion
+}
+
+VARIABLE_STATIC_ALIASES: Dict[str, str] = {
+    "OST": "ostromoukhov_static",
+    "ZF": "zhou_fang_static",
+}
