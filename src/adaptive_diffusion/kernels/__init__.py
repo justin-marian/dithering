b757144d630f7d87b62cd7ee6467ef69428c06e2
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2025-09-13 22:55:50 +0300

    feat: initialize adaptive kernels package
    Signed-off-by: Justin-Marian POPESCU <pmarianjustin@gmail.com>

---
+++ src/adaptive_diffusion/kernels/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import annotations
+
+from .load_kernel import (
+    load_ostro_coeffs,
+    load_zf_strength
+)
+
+__all__ = [
+    "load_ostro_coeffs",
+    "load_zf_strength",
+]
